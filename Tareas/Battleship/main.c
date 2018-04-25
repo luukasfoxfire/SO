f6cb2a1c0a09056df81ef46d34e88bdb59bc0daa
LuukasFoxfire <ichigo.lukas.x@gmail.com>
2018-04-25 20:28:00 -0300

    Added mmap, pipes and forks

--- Tareas/Battleship/main.c
+++ Tareas/Battleship/main.c
@@ -4,8 +4,17 @@
 #include <unistd.h> // for chdir, rmdir and fork and others
 #include <sys/stat.h> // for mkdir
 #include <sys/types.h> //for opendir
-#include <string.h>
+#include <string.h> //for use of strings
+#include <sys/mman.h> //for mmap
 
+//Definimos la forma en la que los usuarios van a interactuar con el tablero
+typedef struct TableTop {
+    int gridForShips[5][5]; //una tabla para los barcos, para evitar que se repitan
+    int ShipsLives[5][2];  //una tabla para saber la cantidad de barcos vivos
+    int gridForAttacks[5][5];  //una tabla para los ataques, para evitar atacar la misma zona dos veces (como en el juego)
+} TableTop;
+
+//Funcion para crear la tabla con las carpetas.
 void createTable(){
     char Path[20];
     char Letters[] = "ABCDE";
@@ -25,7 +34,27 @@ void createTable(){
         }
     }
 
-    printf("We are done already? \nYou are welcome... \n");
+    printf("We are done? ._. \nYou are welcome... \n");
+}
+
+void cleaningTableTops(TableTop *Player){
+    for (int i = 0; i < 5; i++) {
+        for (int x = 0; x < 5; x++) {
+            Player->gridForShips[i][x] = 0;
+            Player->gridForAttacks[i][x] = 0;
+        }
+        for (int x = 0; x < 2; x++) {
+            Player->ShipsLives[i][x] = 0;
+        }
+    }
+}
+
+void positionBoats(pid_t pid){
+    if (pid == 0) {
+        printf("Positioning boats for Player 1\n");
+    } else {
+        printf("Positioning boats for Player 2\n");
+    }
 }
 
 int main(int argc, char const *argv[]) {
@@ -44,20 +73,39 @@ int main(int argc, char const *argv[]) {
             return 1;
         }
         printf("That's not a valid answer. \nPlease press '2'.\n");
-        strcpy(&players, "0");
         fgets(&players, 2, stdin);
 
     }
 
     printf("You have selected %c players. \n", players);
 
-
-
     printf("Creating the entire world of Battleship V2.1 \n");
+
     createTable();
 
-    //pid_t pid;
-    //pid = fork();
+    TableTop *Player1 = (TableTop *)mmap(NULL, sizeof(TableTop), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    TableTop *Player2 = (TableTop *)mmap(NULL, sizeof(TableTop), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    cleaningTableTops(Player1);
+    cleaningTableTops(Player2);
+
+    pid_t pid;
+    pid = fork();
+
+    int PipeForPlayer1[2], PipeForPlayer2[2];
+
+    pipe(PipeForPlayer1);
+    pipe(PipeForPlayer2);
+
+    if (pid == 0) {
+        printf("Child working fine\n");
+        printf("Value of A_1: %d\n", Player1->gridForShips[0][0]);
+    } else {
+        printf("Parent working fine too\n");
+        printf("Value of A_1: %d\n", Player2->gridForShips[0][0]);
+    }
+
+    positionBoats(pid);
 
     return 0;
 }
